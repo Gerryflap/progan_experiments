46fa752cc3b43a68078478c85b6eb26a49484cf1
gerryflap <gerry2211@gmail.com>
2020-05-16 15:08:06 +0200

    Added occasional regularization, which adds regularization only 1/10th of the time, but 10x as strong.

--- train_progan.py
+++ train_progan.py
@@ -31,7 +31,8 @@ def train(
         shuffle=True,
         n_steps_per_output=1000,
         use_special_output_network=False,  # When true: use exponential running avg of weights for G output
-        use_additive_net=False
+        use_additive_net=False,            # Use a network that adds the output of rgb layers together
+        occasional_regularization=False    # Only apply regularization every 10 steps, but 10x as strongly
 ):
     if num_workers == 0:
         print("Using num_workers = 0. It might be useful to add more workers if your machine allows for it.")
@@ -100,19 +101,28 @@ def train(
             # Compute losses
             d_loss = (d_fake_outputs - d_real_outputs).mean()
 
-            size = [s if i == 0 else 1 for i, s in enumerate(fake_batch.size())]
-            eps = torch.rand(size).cuda()
-            x_hat = eps * x + (1.0 - eps) * fake_batch.detach()
-            x_hat.requires_grad = True
-            dis_out = D(x_hat, phase=phase)
-            grad_outputs = torch.ones_like(dis_out)
-            grad = torch.autograd.grad(dis_out, x_hat, create_graph=True, only_inputs=True, grad_outputs=grad_outputs)[
-                0]
-            grad_norm = grad.norm(2, dim=list(range(1, len(grad.size())))) ** 2
-            d_grad_loss = (torch.pow(grad_norm - gamma, 2) / (gamma ** 2)).mean()
+            if (not occasional_regularization) or (n_static_steps_taken % n_static_steps == 0)%10 == 0:
+                size = [s if i == 0 else 1 for i, s in enumerate(fake_batch.size())]
+                eps = torch.rand(size).cuda()
+                x_hat = eps * x + (1.0 - eps) * fake_batch.detach()
+                x_hat.requires_grad = True
+                dis_out = D(x_hat, phase=phase)
+                grad_outputs = torch.ones_like(dis_out)
+                grad = torch.autograd.grad(dis_out, x_hat, create_graph=True, only_inputs=True, grad_outputs=grad_outputs)[
+                    0]
+                grad_norm = grad.norm(2, dim=list(range(1, len(grad.size())))) ** 2
+                d_grad_loss = (torch.pow(grad_norm - gamma, 2) / (gamma ** 2)).mean()
 
-            drift_loss = (d_real_outputs ** 2).mean() + (d_fake_outputs ** 2).mean()
-            d_loss_total = d_loss + 10.0 * d_grad_loss + 0.001 * drift_loss
+                d_reg = 10.0 * d_grad_loss
+
+                drift_loss = (d_real_outputs ** 2).mean() + (d_fake_outputs ** 2).mean()
+                d_reg += 0.001 * drift_loss
+
+                if occasional_regularization:
+                    d_reg = 10.0 * d_reg
+                d_loss_total = d_loss + d_reg
+            else:
+                d_loss_total = d_loss
 
             d_loss_total.backward()
 
@@ -204,5 +214,6 @@ if __name__ == "__main__":
           num_workers=4,
           n_steps_per_output=1000,
           use_special_output_network=True,
-          use_additive_net=False
+          use_additive_net=False,
+          occasional_regularization=True
           )
